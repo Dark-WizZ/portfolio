85630f3a2aee78e1e924ace5987fdda5b12ebaa3
Dark-WizZ <mailwithakram@gmail.com>
2023-07-02 13:08:40 +0530

    Change font

--- src/comp/BgAnimation.jsx
+++ src/comp/BgAnimation.jsx
@@ -1,12 +1,12 @@
 import styles from './BgAnimation.module.scss'
 
 function BgAnimation() {
-  return ( <>
+  return ( <div className='bg-animation'>
     <div id={styles.stars}></div>
     <div id={styles.stars2}></div>
     <div id={styles.stars3}></div>
     <div id={styles.stars4}></div>
-  </> );
+  </div> );
 }
 
 export default BgAnimation;
--- src/comp/Intro.jsx
+++ src/comp/Intro.jsx
@@ -1,7 +1,13 @@
+import side from '../images/side.png'
+
 function Intro() {
   return ( <div className="intro">
     <div className="text">
-    <p>HI im duck</p>
+      <p className="greeting">Hello There! </p>
+      <p className="name">I'm <span>Mohamed Akram</span></p>
+    </div>
+    <div className="image">
+      <img src={side} alt="" />
     </div>
   </div> );
 }
