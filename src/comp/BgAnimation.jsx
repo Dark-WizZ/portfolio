cf8496e51ae4b5e4b0250f4548d4a289f11c9410
Dark-WizZ <mailwithakram@gmail.com>
2023-07-04 07:39:55 +0530

    Add icon resources

--- src/comp/BgAnimation.jsx
+++ src/comp/BgAnimation.jsx
@@ -1,12 +1,12 @@
 import styles from './BgAnimation.module.scss'
 
 function BgAnimation() {
-  return ( <div className='bg-animation'>
+  return ( <>
     <div id={styles.stars}></div>
     <div id={styles.stars2}></div>
     <div id={styles.stars3}></div>
     <div id={styles.stars4}></div>
-  </div> );
+  </> );
 }
 
 export default BgAnimation;
--- src/comp/Skills.jsx
+++ src/comp/Skills.jsx
@@ -1,6 +1,38 @@
+import webpack from '../icons/webpack.svg'
+import css from '../icons/css.svg'
+import js from '../icons/js.svg'
+import html from '../icons/html.svg'
+import git from '../icons/git.svg'
+import npm from '../icons/npm.svg'
+import react from '../icons/react.svg'
+import firebase from '../icons/firebase.svg'
+
 function Skills() {
   return ( <div className="skills">
-    
+    <div className="skill">
+      <img src="" alt="" />
+      <div className="title"></div>
+    </div>
+    <div className="skill">
+      <img src="" alt="" />
+      <div className="title"></div>
+    </div>
+    <div className="skill">
+      <img src="" alt="" />
+      <div className="title"></div>
+    </div>
+    <div className="skill">
+      <img src="" alt="" />
+      <div className="title"></div>
+    </div>
+    <div className="skill">
+      <img src="" alt="" />
+      <div className="title"></div>
+    </div>
+    <div className="skill">
+      <img src="" alt="" />
+      <div className="title"></div>
+    </div>
   </div> );
 }
 
